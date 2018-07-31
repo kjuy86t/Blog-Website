3fd4f057794f28fd574b24b3341c4ebac31dbe63
Ryan Hoffmann <kjuy86t@yahoo.com>
2018-07-31 15:03:13 -0400

    Add files via upload

---
+++ src/pages/07-12-2017-Firtst post/index.md
@@ -0,0 +1,6 @@
+---
+path: "/intro-post"
+date: "2018-07-31T17:12:33.962Z"
+title: "My First Blog Post"	
+---
+This is my first post. In this post I'll introduce what this blog is and what I'm doing with it.
---
+++ src/pages/bungus.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const bungus = () => (
+  <div>
+        <h1>Bungus Chungus Bot</h1>
+        <p>Here you will find all the information you need about the Bungus Chungus discord bot</p>
+        <a href="https://discordbots.org/bot/185124264091844608" >
+            <img src="https://discordbots.org/api/widget/185124264091844608.svg" alt="Bungus Chungus" />
+        </a>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default bungus

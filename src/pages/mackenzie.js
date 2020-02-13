09346d65a98bcc09f18e507640b66e37ab0dda81
Ryan Hoffmann <kjuy86t@yahoo.com>
2020-02-13 12:04:48 -0500

    Add files via upload

--- src/pages/mackenzie.js
+++ src/pages/mackenzie.js
@@ -4,8 +4,13 @@ import Link from 'gatsby-link';
 const mackenzie = () => (
     <div>
         <center>
-            <h1>Will Mackenzie be on time for spanish today</h1>
-            <b>  <p>No</p> </b>
+            <h1>Will Mackenzie be on time for spanish today?</h1>
+            <b>  
+                <font size="8">
+                    <p>No</p>
+                    <img src="./favicon.ico" alt="No" />
+                </font>
+            </b>
         </center>
         <Link to="/">Go back to the homepage</Link>
     </div>

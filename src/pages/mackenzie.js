2bf0c6d2bc6b25efbe5557a5b48a0f8f3a8a2792
Ryan Hoffmann <kjuy86t@yahoo.com>
2020-02-13 12:39:00 -0500

    Add files via upload

--- src/pages/mackenzie.js
+++ src/pages/mackenzie.js
@@ -5,11 +5,13 @@ const mackenzie = () => (
     <div>
         <center>
             <h1>Will Mackenzie be on time for spanish today?</h1>
+            <h1>""
+            </h1>
             <b>  
                 <font size="8">
                     <p>No</p>
-                    <img src="./favicon.ico" alt="No" />
                 </font>
+                    <img src="https://images.discordapp.net/avatars/185124264091844608/d027d02b9deb091fa0e0f9a11c3a4151.png?size=256" alt="No" />
             </b>
         </center>
         <Link to="/">Go back to the homepage</Link>

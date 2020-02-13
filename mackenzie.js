a8a0d8a07fd14d2c3aad747db47fa260200fff20
Ryan Hoffmann <kjuy86t@yahoo.com>
2020-02-13 12:11:35 -0500

    Add files via upload

---
+++ mackenzie.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const mackenzie = () => (
+    <div>
+        <center>
+            <h1>Will Mackenzie be on time for spanish today?</h1>
+            <b>  
+                <font size="8">
+                    <p>No</p>
+                    <img src="../favicon.ico" alt="No" />
+                </font>
+            </b>
+        </center>
+        <Link to="/">Go back to the homepage</Link>
+    </div>
+)
+
+export default mackenzie;

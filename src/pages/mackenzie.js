1775f921183c2f1f77f135c252abd10a35cc84b0
Ryan Hoffmann <kjuy86t@yahoo.com>
2020-02-13 11:57:23 -0500

    Add files via upload

---
+++ src/pages/mackenzie.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const mackenzie = () => (
+    <div>
+        <center>
+            <h1>Will Mackenzie be on time for spanish today</h1>
+            <b>  <p>No</p> </b>
+        </center>
+        <Link to="/">Go back to the homepage</Link>
+    </div>
+)
+
+export default mackenzie;
